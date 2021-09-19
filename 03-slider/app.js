7a9ffbd14f7164e2679f1178b59677737d8252ae
Kazakov Stepan <ksrt12group@gmail.com>
2021-09-19 19:37:19 +0300

    Project 3: Slides
    Signed-off-by: Kazakov Stepan <ksrt12group@gmail.com>

---
+++ 03-slider/app.js
@@ -0,0 +1,32 @@
+const upBtn = document.querySelector(".up-button");
+const downBtn = document.querySelector(".down-button");
+const sidebar = document.querySelector(".sidebar");
+const container = document.querySelector(".container");
+const mainSlide = document.querySelector(".main-slide");
+const slidesCount = mainSlide.childElementCount;
+
+let activeSlideIndex = 0;
+
+sidebar.style.top = `-${(slidesCount - 1) * 100}vh`;
+
+upBtn.addEventListener("click", () => changeSlide(true));
+downBtn.addEventListener("click", () => changeSlide(false));
+
+function changeSlide(up) {
+    if (up) {
+        activeSlideIndex += 1;
+        if (activeSlideIndex === slidesCount) {
+            activeSlideIndex = 0;
+        }
+    } else {
+        activeSlideIndex -= 1;
+        if (activeSlideIndex < 0) {
+            activeSlideIndex = slidesCount - 1;
+        }
+    }
+
+    const height = container.clientHeight;
+
+    mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`;
+    sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`;
+}
